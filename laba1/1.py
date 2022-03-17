1ff62b8d89c4e261b116d88df79b778c5c87fd37
1Kuper111 <101783735+1Kuper111@users.noreply.github.com>
2022-03-17 14:08:07 +0600

    Add files via upload

---
+++ laba1/1.py
@@ -0,0 +1,15 @@
+a=input("Введите количество:")
+print(a)
+a=int(input())
+a=float(input())
+a=3
+b=1 
+print(a)
+print(a+b)
+print('сумма=',a+b)
+a=3; b=1; print(a)
+print(a+b)
+print('сумма=',a+b)
+x=2
+y=5
+print(x,"+",y,"=",x+y,sep=" ")
